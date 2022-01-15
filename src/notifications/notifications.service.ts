270528aadb763f31196dce48b3d46a834fed11e3
Alvaro <alvaronavarromora@gmail.com>
2022-01-15 19:39:09 +0100

    change ms notification logic

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -3,6 +3,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { Categories } from 'src/enums/categories';
 import { SuscriptionsService } from 'src/suscriptions/suscriptions.service';
 import CreateNotificationDto from './dto/createNotification.dto';
+import axios from 'axios';
 
 @Injectable()
 export class NotificationsService {
@@ -12,15 +13,51 @@ export class NotificationsService {
   ) {}
 
   async notify(notification: CreateNotificationDto): Promise<boolean> {
-    const subscribers = await this.suscriptionsService.findAll('', notification.topic, notification.category);
+    var response;
+    var title = '';
+    var message = '';
+    var topics;
+    if ( notification.category == Categories.Pelicula){
+      response = await this.getFilm(notification.referenceId)
+      topics = response.data.genre;
+      title = response.data.title;
+    }else{
+      var response = await this.getNew(notification.referenceId);
+      topics = response.data.genre;
+      title = response.data.title;
+      message = "Hola! Se ha subido una nueva noticia con tílulo \""+title+"\", que tiene la etiqueta "
+      + topics + " a la que estás suscrito/a.";
+    }
     var messagesSent = true;
-    for ( var subscriber of subscribers){
-      if (! await this.send({ email: subscriber.mail, subject: subscriber.subject, name: "TODO NOMBRE", customMessage: 'TODO MENSAJE'})){
-        messagesSent = false;
+    for ( var topic of topics){
+      if ( notification.category == Categories.Pelicula){
+        message = "Hola! Se ha subido una nueva película con tílulo \""+title+"\", que pertenece al género "
+        + topic + " a el que estás suscrito/a.";
+      }else{
+        message = "Hola! Se ha subido una nueva noticia con tílulo \""+title+"\", que tiene la etiqueta "
+        + topic + " a la que estás suscrito/a.";
+      }
+      const subscribers = await this.suscriptionsService.findAll('', topic, notification.category);
+      for ( var subscriber of subscribers){
+        if (! await this.send({ email: subscriber.mail, subject: "Rottenpotatoes " + subscriber.subject, name: "", customMessage: message})){
+          messagesSent = false;
+        }
       }
     }
     
     return Promise.resolve(messagesSent);
+  }
+
+  async getFilm(filmId) {
+    axios.get(
+      "https://api-drorganvidez.cloud.okteto.net/api/v1/films/"+filmId,
+    ).then((res) => res).catch((error) => error.response);
+  }
+
+  async getNew(newId) {
+    return axios.get(
+      "https://api-fis-josenggn.cloud.okteto.net/api/v1/news/"+newId,
+    ).then((res) => res).catch((error) => error.response);
   }
 
   async send({ email: to, subject, name, customMessage }): Promise<boolean> {
