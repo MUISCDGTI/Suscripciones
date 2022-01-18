fcdb9be5413376ce5f892c3684b12d7d22585b4b
Alvaro <48862983+AlvaroNavarroMora@users.noreply.github.com>
2022-01-18 10:00:04 +0100

    add master apikey

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,16 +6,18 @@ import axios from 'axios';
 export class AppService {
   async verifyApiKey(apikey: string) : Promise<boolean>{
     var output = false;
-    var response = await axios.post(
-        "https://api-gfpedro.cloud.okteto.net/isAuthenticated",
-        {token:apikey},
-      ).then((res) => res).catch((error) => error.response);
-    
-    if(response?.status == 204){
-        output=true;
-    }
-console.log(output);
-    return Promise.resolve(output);
+    if (apikey == "rottenpotatoes"){
+      output = true;
+    else{
+      var response = await axios.post(
+          "https://api-gfpedro.cloud.okteto.net/isAuthenticated",
+          {token:apikey},
+        ).then((res) => res).catch((error) => error.response);
 
+      if(response?.status == 204){
+          output=true;
+      }
+    }
+    return Promise.resolve(output);
   }
 }
